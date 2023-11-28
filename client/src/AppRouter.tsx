2eb08d48ad0d09746097aef1d5758e800d0a49bb
aagrawal1022 <76031088+aagrawal1022@users.noreply.github.com>
2023-11-29 00:46:20 +0530

    configured typescript for react

---
+++ client/src/AppRouter.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AppRouter = () => {
+  return (
+    <div>
+      <h1>Hello, Welcome to React and TypeScript</h1>
+    </div>
+  );
+};
+
+export default AppRouter;
